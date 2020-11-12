f007afa7d8a24dbd73b6cc8af79e18a8f8e56589
raquelmvz <raquel.mvz98@gmail.com>
2020-11-12 11:04:30 +0100

    Metodos autentificacion, menuSalir y pedirMetrosDependencia

--- src/main/java/programa/Aspiradaw.java
+++ src/main/java/programa/Aspiradaw.java
@@ -13,15 +13,24 @@ import javax.swing.JOptionPane;
  */
 public class Aspiradaw {
 
-    //Metodo que solicita un usuario y contraseña hasta que se introduce correctamente
+    //Declaracion de todas las variables a utilizar a lo largo del programa
     //El usuario y la contraseña se guardan como final
+    static final String USUARIO = "raquel";
+    static final String PASSWORD = "aspiradaw";
+    static boolean repetir = true; // Para que el programa se repita siempre a no ser que salgamos del programa 
+    static final double PORCENTAJE_PIERDE_ASPIRANDO = -0.15;
+    static final double PORCENTAJE_PIERDE_FREGANDO = -0.225;
+    static final double MINIMO_BATERIA = 0.3;
+
+    //Metodo que solicita un usuario y contraseña hasta que se introduce correctamente
     public static void autentificacion() {
 
-        final String USUARIO = "alumno";
-        final String PASSWORD = "aspiradaw";
         String user = JOptionPane.showInputDialog("Introduce tu usuario:");
         String pass = JOptionPane.showInputDialog("Introduce tu contraseña:");
 
+        //Si la primera vez que se introducen los datos éstos son erróneos sale una ventana
+        //de error diciendo que no son válidos
+        //Se vuelve a solicitar hasta que sean correctos
         while (!user.equals(USUARIO) || !pass.equals(PASSWORD)) {
             JOptionPane.showMessageDialog(null, "Usuario o contraseña no válidos");
             user = JOptionPane.showInputDialog("Introduce tu usuario:");
@@ -30,11 +39,27 @@ public class Aspiradaw {
 
     }
 
+    //Metodo que pregunta si deseas salir del programa
+    public static boolean menuSalir() {
+        //Se pregunta si quieres salir
+        //Devuelve 0 o 1 dependiendo del YES o NO
+        int opcion = JOptionPane.showConfirmDialog(null, "¿Salir del programa?", "Salida del programa", JOptionPane.YES_NO_OPTION);
+
+        if (opcion == JOptionPane.YES_OPTION) {
+            //Si marcamos YES
+            repetir = false; // Si repetir es false paramos la ejecucion del programa
+
+        }
+
+        return repetir;
+    }
+
     // Método que solicite los metros cuadrados de una dependencia con JOption
     public static double pedirMetrosDependencia() {
 
         double metrosCuadradosDependencia = 0;
 
+        //Los valores de metros no pueden ser ni inferior a 1 ni superiores a 100
         do {
 
             String metrosDependencia = JOptionPane.showInputDialog("Introduce los metros cuadrados de la dependencia\n"
@@ -49,20 +74,13 @@ public class Aspiradaw {
     // Metodo que crea un array vacio de una longitud determinada
     // Para cada estancia vamos a crear un array vacio para poder guardar
     // los metros cuadrados de cada habitacion de esa dependencia
-    public static int[] devuelveArray(int longitud) {
-
-        int[] contadorCadaDependencia = new int[longitud];
-
-        return contadorCadaDependencia;
-    }
-
+//    public static int[] devuelveArray(int longitud) {
+//
+//        int[] contadorCadaDependencia = new int[longitud];
+//
+//        return contadorCadaDependencia;
+//    }
     public static void main(String[] args) {
-
-        // Declaracion de variables
-        boolean repetir = true; // Para que el programa se repita siempre a no ser que salgamos del programa 
-        final double PORCENTAJE_PIERDE_ASPIRANDO = -0.15;
-        final double PORCENTAJE_PIERDE_FREGANDO = -0.225;
-        final double MINIMO_BATERIA = 0.3;
 
         //AUTENTIFICACION MEDIANTE USUARIO Y CONTRASEÑA
         autentificacion();
@@ -96,15 +114,7 @@ public class Aspiradaw {
             JOptionPane.showMessageDialog(null, "DORMITORIO 2");
             double metrosCuadradosDorm2 = pedirMetrosDependencia();
 
-            //Se pregunta si quieres salir
-            //Devuelve 0 o 1 dependiendo del YES o NO
-            int op = JOptionPane.showConfirmDialog(null, "¿Deseas salir?", "Salida del programa", JOptionPane.YES_NO_OPTION);
-
-            if (op == JOptionPane.YES_OPTION) {
-                //Si marcamos YES
-                repetir = false; // Si repetir es false paramos la ejecucion del programa
-
-            }
+            menuSalir();
 
         } while (repetir);
 
